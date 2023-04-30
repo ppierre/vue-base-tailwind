b308f0c7ae29a8106108d8b3ae7345949abf34a1
pierre pracht <pierre.pracht@gmail.com>
2023-04-30 14:05:44 +0200

    Liste avec répetions

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,21 @@
   </header>
 
   <main>
+    <div class="personne-card border-2 p-1">
+      <p>nom : Jean</p>
+      <p>age : 22</p>
+      <p>marié : non</p>
+    </div>
+    <div class="personne-card border-2 p-1">
+      <p>nom : Paul</p>
+      <p>age : 25</p>
+      <p>marié : oui</p>
+    </div>
+    <div class="personne-card border-2 p-1">
+      <p>nom : Renée</p>
+      <p>age : 33</p>
+      <p>marié : non</p>
+    </div>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora recusandae et dolorem odit
       beatae illo labore, praesentium quidem nulla eveniet, at voluptatem incidunt. Eaque explicabo,
