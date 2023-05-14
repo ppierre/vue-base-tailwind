ee4c9af36d48a0a922eb7c23e74ae539d3c216ca
pierre pracht <pierre.pracht@gmail.com>
2023-05-14 14:28:32 +0200

    bump deps

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import routes from '~pages'
+import { createRouter, createWebHistory } from 'vue-router'
 
 import './index.css'
 
-createApp(App).mount('#app')
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
